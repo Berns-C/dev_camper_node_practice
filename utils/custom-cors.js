76e9a075231d9fa38063e35df5ba23c8d1281d25
Bernhard Paolo <bernhardpaolocena@gmail.com>
2021-03-14 23:14:48 +0800

    updated the cors config settings

--- utils/custom-cors.js
+++ utils/custom-cors.js
@@ -2,11 +2,8 @@ const corsSettings = (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Expose-Headers", "Content-Length");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,DELETE,POST,PUT");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, Authorization, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method"
-    );
+    res.setHeader("Access-Control-Allow-Methods", process.env.CORS_ALLOW_METHODS);
+    res.setHeader("Access-Control-Allow-Headers", process.env.CORS_ALLOW_HEADERS);
     next();
 }
 
